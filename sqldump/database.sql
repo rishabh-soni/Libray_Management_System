51183b3a1aadbe9c8f3d3adf62fea368ff111e49
rishabh-soni <rishabh.rs.001@gmail.com>
2021-04-21 19:22:40 +0530

    Added  the database.sql file

--- sqldump/database.sql
+++ sqldump/database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lms`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: lms
@@ -352,4 +354,4 @@ CREATE TABLE `django_session` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-21 18:59:16
+-- Dump completed on 2021-04-21 19:20:28
